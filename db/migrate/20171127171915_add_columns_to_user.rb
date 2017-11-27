a7091557ec6be325413767c211e0c0e2ed89e522
Antoine Tourasse <antoine.tourasse@gmail.com>
2017-11-27 18:22:27 +0100

    Model user done

---
+++ db/migrate/20171127171915_add_columns_to_user.rb
@@ -0,0 +1,8 @@
+class AddColumnsToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :address, :string
+    add_column :users, :phone_number, :string
+  end
+end
