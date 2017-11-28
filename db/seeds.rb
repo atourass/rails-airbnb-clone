38c31d3fc368f40d3bba042093a4970cb06f31a2
jeanluc <contact@forummc.com>
2017-11-28 18:00:35 +0000

    seed vehicle

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Vehicle.destroy_all
+User.destroy_all
+
+require 'faker'
+
+1.times do
+  name = Faker::FamilyGuy.character.delete(' ')
+  user = User.new(
+    email: "#{name}@#{Faker::Music.instrument.delete(' ')}.com",
+    password: "jeanluc",
+    first_name: "John",
+    last_name: name,
+    address: "#{Faker::FamilyGuy.location}.com"
+  )
+  user.save!
+  # puts user.errors.full_messages
+  #
+  puts 'Creating x fake VEHICLES...'
+
+  (1..3).to_a.sample.times do
+    vehicle = Vehicle.new(
+      brand:  "HONDA",
+      price:  (25..100).to_a.sample,
+      description: "Scooter",
+      model: %w(125 250 350 500 750).to_a.sample
+    )
+    vehicle.user = user
+    puts vehicle
+    vehicle.save
+  end
+end
+
+
+puts 'Finished!'
