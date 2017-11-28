011e6bdf6eccd2b4f22f44c551604b6c28996f81
Antoine Tourasse <31624561+tarzoom@users.noreply.github.com>
2017-11-28 12:18:31 +0100

    Update vehicle.rb

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,4 +1,4 @@
 class Vehicle < ApplicationRecord
   belongs_to :user
-  validates :price, :description, :brand, presence: :true, allow_blank: :false
+  validates :price, :description, :brand, :model, presence: :true, allow_blank: :false
 end
