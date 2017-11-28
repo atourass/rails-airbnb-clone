3a05ebd79119706c0f372adcfe2c6019376c8a53
Antoine Tourasse <31624561+tarzoom@users.noreply.github.com>
2017-11-28 18:21:20 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,30 +9,19 @@ Vehicle.destroy_all
 User.destroy_all
 
 require 'faker'
-
-1.times do
-  name = Faker::FamilyGuy.character.delete(' ')
-  user = User.new(
-    email: "#{name}@#{Faker::Music.instrument.delete(' ')}.com",
-    password: "jeanluc",
-    first_name: "John",
-    last_name: name,
-    address: "#{Faker::FamilyGuy.location}.com"
+BRANDS = %w(Honda, Suzuki, Harley, BMW, Kawasaki)
+10.times do
+  user = User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(8)
   )
-  user.save!
-  # puts user.errors.full_messages
-  #
-  puts 'Creating x fake VEHICLES...'
-
-  (1..3).to_a.sample.times do
-    vehicle = Vehicle.new(
-      brand:  "HONDA",
-      price:  (25..100).to_a.sample,
-      description: "Scooter",
-      model: %w(125 250 350 500 750).to_a.sample
-    )
+  vehicle = Vehicle.new(
+    brand:  BRANDS.sample,
+    price:  (25..100).to_a.sample,
+    description: "Trop bien",
+    model: %w(125 250 350 500 750).to_a.sample
+  )
     vehicle.user = user
-    puts vehicle
     vehicle.save
   end
 end
