cd556744757e8b3a1093d1998d682789203aa491
Elena Maroto <elena_maroto@hotmail.com>
2017-11-29 12:14:12 +0100

    Show vehicle view

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,15 @@ Rails.application.routes.draw do
   root to: 'vehicles#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :vehicles do
-    member do
-      resources :bookings, only: :index
-    end
-  end
+  resources :vehicles, only: [:index, :show]
+  # do
+  #   member do
+  #     resources :bookings, only: :index
+  #   end
+  # end
 
-  resources :bookings, except: [:index, :destroy]
+  # resources :bookings, except: [:index, :destroy]
 
-  resources :dashboards, only: :index
+  # resources :dashboards, only: :index
 
 end
