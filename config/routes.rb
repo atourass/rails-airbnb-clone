7250a992a3f879ecaa091d0a1124464b3a778df8
Antoine Tourasse <antoine.tourasse@gmail.com>
2017-11-29 12:27:27 +0100

    View basique + modifs model + schema. xml

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
-  mount Attachinary::Engine => "/attachinary"
   devise_for :users
   root to: 'vehicles#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :vehicles do
+  resources :vehicles, only: [:index] do
     member do
-      resources :bookings, only: :index
+      resources :bookings, only: [:index, :new, :create]
     end
   end
 
-  resources :bookings, except: [:index, :destroy]
+  # resources :bookings, e: [:index, :destroy]
 
-  resources :dashboards, only: :index
-
+  # resources :dashboards, only: :index
+  mount Attachinary::Engine => "/attachinary"
 end
